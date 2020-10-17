c993929c1885fe50605fd9f492113c49a65c25bc
Franco Benitez <franco_casla_benitez@hotmail.com>
2020-10-17 16:51:11 -0300

    update

---
+++ build/precache-manifest.bdb6281c0e08b90a67cb5bbba35e9ff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0dd60b3839da80b939eaf9db7dcf99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1681296ac8db3d5552c7",
+    "url": "/static/css/main.6547b84b.chunk.css"
+  },
+  {
+    "revision": "145727f91d40263be6cb",
+    "url": "/static/js/2.e246d17e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e246d17e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1681296ac8db3d5552c7",
+    "url": "/static/js/main.b25ef98c.chunk.js"
+  },
+  {
+    "revision": "66efed4028d5c3ffdcb3",
+    "url": "/static/js/runtime-main.64c1b557.js"
+  },
+  {
+    "revision": "1f7f94c38a5121fdc493d0b7f8a5f587",
+    "url": "/static/media/logo-mascota.1f7f94c3.png"
+  }
+]);
